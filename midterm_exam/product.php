f3da75025a6edf8bfeb2077bcb5e7fe9529a498c
kanyarat031044 <65112479@dpu.ac.th>
2022-10-30 16:56:06 +0700

    nothwind

--- midterm_exam/product.php
+++ midterm_exam/product.php
@@ -1,23 +1,37 @@
+<?php
+include 'db_connect.php';
+
+          $sql = 'select * from categories';
+$result = $conn->query($sql);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Products</title>
+  <title>Search product by category</title>
 </head>
 <body>
-  <table>
-    <tr>
-      <th>Product name</th>
-      <th>Units in stock</th>
-    </tr>
-    <!-- add table rows hear ex.
-    <tr>
-      <td>Chai</td>
-      <td>39</td>
-    </tr>
-    -->    
-  </table>
+  <header>
+    <form action="product.php" method="get">
+      <label for="category">
+        <select name="category" id="category">
+          <!-- add options hear ex.
+          <option value="1">Beverages</option>
+          -->
+          
+
+
+    <?php while($row = $result->fetch_assoc()) {?>
+    <option value="<?php echo $row["CategoryID"];?>">
+      <?php echo $row["CategoryName"]; ?>
+      </option>
+    <?php } ?>
+        </select>
+      </label>
+      <input type="submit" value="Search" name="submit">
+    </form>
+  </header>
 </body>
 </html>
--- midterm_exam/search.php
+++ midterm_exam/search.php
@@ -1,23 +1,36 @@
+<?php
+include 'db_connect.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Search product by category</title>
+  <title>Products</title>
 </head>
 <body>
-  <header>
-    <form action="product.php" method="get">
-      <label for="category">
-        <select name="category" id="category">
-          <!-- add options hear ex.
-          <option value="1">Beverages</option>
-          -->
-        </select>
-      </label>
-      <input type="submit" value="Search" name="submit">
-    </form>
-  </header>
+  <?php
+$sql = "SELECT * FROM products where CategoryID=1";
+    $result = $conn->query($sql);
+    ?>
+<div class="container" boder='1'>
+        
+        <table>
+            <thead>
+                <tr>
+                    
+                    <td width="25%">productname</td>
+                    <td width="25%">Units in stock</td>
+                </tr>
+            </thead>
+            <?php while($row = $result->fetch_assoc()): ?>
+  <tr>
+   <td><?php echo $row['ProductName']; ?></td>
+   <td><?php echo $row['UnitsInStock']; ?></td>
+   </tr>
+ <?php endwhile ?>
+            </tbody>
+        </table>
 </body>
 </html>
